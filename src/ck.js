323549ec1976925e79f9845f24832bb972314e05
Anthony Gore <small.iron6149@fastmail.com>
2022-11-11 15:25:31 +1100

    Update ck.js

--- src/ck.js
+++ src/ck.js
@@ -1,7 +1,7 @@
 import { CourseKitClient } from '@coursekit/client'
 const ck = new CourseKitClient({
   schoolId: process.env.REACT_APP_SCHOOL_ID,
-  baseUrl: 'https://api2.coursekit.dev/v1',
+  baseUrl: 'https://api.coursekit.dev/v1',
 })
 
 const fetchUser = async () => {
